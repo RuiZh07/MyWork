56659e298743216827db29ff4eaeadc8c076fae4
oso95 <cyw@cywang.me>
2022-12-12 00:04:30 -0600

    add test function

--- scripts/writer.js
+++ scripts/writer.js
@@ -1,8 +1,20 @@
 const ndef = new NFEFReader();
-try {
-    await ndef.write({
-        records: [{ recordType: "url", data: "https://themaverse.io/"}]
-    });
-} catch {
-    console.log("Write failed, Please try again!")
-};
+
+async function writeNFC(){
+    try {
+        await ndef.write({
+            records: [{ recordType: "url", data: "https://themaverse.io/"}]
+        });
+    } catch {
+        alert("Write failed, Please try again!")
+    };
+}
+
+function getID(){
+    var text=document.getElementById('input1').value;
+    window.alert(text)
+}
+
+function test(){
+    alert("something");
+}
