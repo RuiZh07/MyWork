c3eeec37536c80b8bd34bbdf73eed6a6e3cffaa3
oso95 <cyw@cywang.me>
2022-12-22 16:40:12 -0600

    Update main.go

--- main.go
+++ main.go
@@ -1,61 +1,3 @@
-// // This is the name of our package
-// // Everything with this package name can see everything
-// // else inside the same package, regardless of the file they are in
-// package main
-
-// // These are the libraries we are going to use
-// // Both "fmt" and "net" are part of the Go standard library
-// import (
-// 	// "fmt" has methods for formatted I/O operations (like printing to the console)
-// 	//"fmt"
-// 	// The "net/http" library has methods to implement HTTP clients and servers
-// 	"net/http"
-// 	"html/template"
-// 	//"github.com/gin-gonic/gin"
-// )
-
-// var tmpl *template.Template
-
-// func main() {
-
-// 	http.HandleFunc("/", index)
-// 	http.HandleFunc("/signupPage", signupPage)
-// 	http.HandleFunc("/signup", signup)
-// 	http.ListenAndServe(":8080", nil)
-// }
-
-// func init() {
-//     tmpl = template.Must(template.ParseGlob("templates/*.html"))
-// }
-
-// func index(reswt http.ResponseWriter, req *http.Request) {
-//     tmpl.ExecuteTemplate(reswt, "index.html", nil)
-// }
-
-// func signupPage(w http.ResponseWriter, r *http.Request){
-// 	tmpl.ExecuteTemplate(w, "signup.html", nil)
-// }
-
-// func signup(w http.ResponseWriter, r *http.Request){
-// 	if r.Method != "POST" {
-// 		http.Redirect(w, r, "/", http.StatusSeeOther)
-// 		return
-// 	}
-// 	userName := r.FormValue("username")
-// 	password := r.FormValue("password")
-
-// 	d := struct{
-
-// 		Username string
-// 		Password string
-// 	}{
-// 		Username: userName,
-// 		Password: password,
-// 	}
-
-// 	tmpl.ExecuteTemplate(w, "sub.html", d)
-// }
-
 package main
 
 import (
@@ -76,6 +18,8 @@ func main(){
 
 	// Routes
 	app.Get("/", index)
+	app.Get("/signupPage", loadSignUp)
+	app.Get("/loginPage", loadLogin)
 
 	// Start server
 	log.Fatal(app.Listen(":8080"))
@@ -86,3 +30,10 @@ func index(c *fiber.Ctx) error {
 	return c.Render("index", nil)
 }
 
+func loadLogin(c *fiber.Ctx) error {
+	return c.Render("login", nil)
+}
+
+func loadSignUp(c *fiber.Ctx) error {
+	return c.Render("signup", nil)
+}
