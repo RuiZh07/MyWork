2af83842a6ba11f8134a301908166c0e32b8cb92
“oso95” <root@ip-172-31-93-42.ec2.internal>
2022-12-28 00:36:14 +0000

    fixed overwritten issue when displaying university name

--- controller/signup.go
+++ controller/signup.go
@@ -2,13 +2,13 @@ package controller
 
 import (
 	"database/sql"
+	"encoding/json"
+	"github.com/gofiber/fiber/v2"
 	_ "github.com/lib/pq"
-    "log"
-    "golang.org/x/crypto/bcrypt"
-    "github.com/gofiber/fiber/v2"
-    "encoding/json"
+	"golang.org/x/crypto/bcrypt"
 	"io/ioutil"
-    "strings"
+	"log"
+	"strings"
 )
 
 type UniversityData struct {
@@ -20,122 +20,100 @@ type UniversityData struct {
 
 var db *sql.DB
 
-var university string
+var universitySelected string
 var domain string
 
 func init() {
-    var err error
-    // Install postgresDB in your machine and change the `admin:admin` with your `username:password` and change `wacave` with your database name
-    // make sure you create table in your database with following code
-    // CREATE TABLE users (
-    //     id serial PRIMARY KEY,
-    //     email text NOT NULL,
-    //     password text NOT NULL,
-    //     university text NOT NULL
-    // );
-    
-    db, err = sql.Open("postgres", "postgres://admin:admin@localhost:5432/wacave?sslmode=disable")
-    if err != nil {
-        log.Fatal(err)
-    }
-}
+	var err error
+	// Install postgresDB in your machine and change the `admin:admin` with your `username:password` and change `wacave` with your database name
+	// make sure you create table in your database with following code
+	// CREATE TABLE users (
+	//     id serial PRIMARY KEY,
+	//     email text NOT NULL,
+	//     password text NOT NULL,
+	//     university text NOT NULL
+	// );
 
+	db, err = sql.Open("postgres", "postgres://admin:admin@localhost:5432/wacave?sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func HandleRegistration(c *fiber.Ctx) error {
-    // Get the form values
-    email := c.FormValue("email")
-    password := c.FormValue("password")
+	// Get the form values
+	email := c.FormValue("email")
+	password := c.FormValue("password")
 
-    if !strings.Contains(email, ".edu") {
-        return c.Render("signup", fiber.Map{
-            "UniversitySelected": university, 
-            "UniversityDomain": domain,
-            "ErrorMessage": "Email Domain Not Supported",
-        })
-    }
+	if !strings.Contains(email, ".edu") {
 
-    // Hash the password
-    hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-    if err != nil {
-        return err
-    }
+		return c.Render("signup", fiber.Map{
+			"UniversityDomain": domain,
+			"ErrorMessage":     "Email Domain Not Supported",
+		})
+	}
 
-    // Check if the email is already in use
-    var count int
-    err = db.QueryRow("SELECT COUNT(*) FROM users WHERE email = $1", email).Scan(&count)
-    if err != nil {
-        return err
-    }
-    if count > 0 {
-        // Email is already in use, return an error
-        return c.Render("signup", fiber.Map{
-            "UniversitySelected": university, 
-            "UniversityDomain": domain,
-            "ErrorMessage": "Email is already in use",
-        })
-    }
-
-    // Insert the new user into the database
-    _, err = db.Exec("INSERT INTO users (email, password, university) VALUES ($1, $2, $3)", email, hashedPassword, university)
-    if err != nil {
-        return err
-    }
-
-    c.Redirect("/login")
-    return nil
-}
-
-func getUniversities(db *sql.DB) ([]string, error) {
-
-	rows, err := db.Query("SELECT name FROM universities")
+	// Hash the password
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
-		return nil, err
+		return err
 	}
-	defer rows.Close()
 
-	var universities []string
-	for rows.Next() {
-		var name string
-		if err := rows.Scan(&name); err != nil {
-			return nil, err
-		}
-		universities = append(universities, name)
+	// Check if the email is already in use
+	var count int
+	err = db.QueryRow("SELECT COUNT(*) FROM users WHERE email = $1", email).Scan(&count)
+	if err != nil {
+		return err
 	}
-	return universities, nil
+	if count > 0 {
+		// Email is already in use, return an error
+		return c.Render("signup", fiber.Map{
+			"UniversityDomain": domain,
+			"ErrorMessage":     "Email is already in use",
+		})
+	}
+
+	// Insert the new user into the database
+	_, err = db.Exec("INSERT INTO users (email, password, university) VALUES ($1, $2, $3)", email, hashedPassword, universitySelected)
+	if err != nil {
+		return err
+	}
+
+	c.Redirect("/login")
+	return nil
 }
 
+func LoadRegister(c *fiber.Ctx) error {
 
-func LoadRegister(c *fiber.Ctx) error{
-	
-    dataJSON, err := ioutil.ReadFile("data/universityData.json")
-    if err != nil {
-        log.Fatal(err)
-    }
-    
-    // Unmarshal the JSON data into a slice of UniversityData structs.
+	dataJSON, err := ioutil.ReadFile("data/universityData.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Unmarshal the JSON data into a slice of UniversityData structs.
 	var data []UniversityData
 	err = json.Unmarshal(dataJSON, &data)
 	if err != nil {
 		log.Fatal(err)
 	}
-    var uName []string
-    for _, university := range data {
-        uName = append(uName, university.Name)
-    }
+	var uName []string
+	for _, university := range data {
+		uName = append(uName, university.Name)
+	}
 
-    return c.Render("universitySelectRegister", uName)
-	
+	return c.Render("universitySelectRegister", uName)
+
 }
 
-func HandleUniversitySelection(c *fiber.Ctx) error{
-    university = c.FormValue("university")
-    err := db.QueryRow("SELECT domain FROM universities WHERE name = $1", university).Scan(&domain)
+func HandleUniversitySelection(c *fiber.Ctx) error {
+	universitySelected = c.FormValue("university")
+	err := db.QueryRow("SELECT domain FROM universities WHERE name = $1", universitySelected).Scan(&domain)
 	if err != nil {
 		return err
 	}
 
-    return c.Render("signup", fiber.Map{
-        "UniversitySelected": university, 
-        "UniversityDomain": domain,
-    })
+	return c.Render("signup", fiber.Map{
+		"UniversityDomain": domain,
+	})
+
 }
--- main.go
+++ main.go
@@ -3,14 +3,14 @@ package main
 import (
 	"log"
 	// "time"
-	"database/sql"
-	_ "github.com/lib/pq"
-	"github.com/gofiber/template/html"
-	"github.com/gofiber/fiber/v2"
 	"NFC_Tag_UPoint/controller"
+	"database/sql"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html"
+	_ "github.com/lib/pq"
 )
 
-func main(){
+func main() {
 
 	// log.Println("Waitting for server to boot in 10s")
 	// time.Sleep(10 * time.Second)
@@ -20,7 +20,7 @@ func main(){
 
 	// Initialize standard go html template engine
 	engine := html.New("./templates", ".html")
-	
+
 	// Fiber instance
 	app := fiber.New(fiber.Config{
 		Views: engine,
@@ -34,8 +34,6 @@ func main(){
 	app.Post("/register", controller.HandleRegistration)
 	app.Post("/login", controller.HandleLogin)
 
-
-
 	// Start server
 	log.Fatal(app.Listen(":8080"))
 }
@@ -45,7 +43,7 @@ func index(c *fiber.Ctx) error {
 	return c.Render("index", nil)
 }
 
-func createTable(){
+func createTable() {
 	// Connect to the PostgreSQL server.
 	db, err := sql.Open("postgres", "postgres://admin:admin@localhost:5432/wacave?sslmode=disable")
 	if err != nil {
@@ -79,4 +77,3 @@ func createTable(){
 		log.Fatal(err)
 	}
 }
-
