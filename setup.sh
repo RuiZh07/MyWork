4fc756f5400535451145694ef0ad3a7517406005
root <root@ip-172-31-88-173.ec2.internal>
2023-01-03 06:46:00 +0000

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -9,24 +9,26 @@ snap install go --classic
 # Install postgreSQL
 echo "Installing postgreSQL"
 sudo apt-get update
-sudo apt install postgresql postgresql-contrib
+sudo apt install postgresql postgresql-contrib postgresql-client
 
 # Starting database
+echo "Starting Database"
 sudo service postgresql start
-sudo -u postgres psql
 
 # Creating user "admin" with password "admin"
-echo "Creating user 'admin' with password 'admin'"
+echo "Creating user admin with password 'admin' "
 sudo -u postgres psql -c "CREATE USER admin WITH PASSWORD 'admin';"
 
 # Creating database "wacave"
 echo "Creating database 'wacave'"
 sudo -u postgres psql -c "CREATE DATABASE wacave;"
 
+
 # Grant all permission to "admin" for "wacave" database
 echo "Granting all permissions to user 'admin' for database 'wacave'"
 sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE wacave TO admin;"
 
 # Starting web service
+echo "Starting web server"
 go mod tidy
 go run main.go
