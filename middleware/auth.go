f1f9551041e70953b1032a0cf1e95de003e77305
Ubuntu <cyw@cywang.me>
2023-01-13 05:23:49 +0000

    added condition to redirect user already signed in

--- middleware/auth.go
+++ middleware/auth.go
@@ -10,6 +10,8 @@ func setAuth() fiber.Handler {
 	return noAuth
 }
 
+// This is to check if user has session AUTH_KEY in their cookie, redirect
+// user to /user/dashboard if they already signed in
 func noAuth(c *fiber.Ctx) error {
     sess, err := store.Get(c)
 
@@ -17,21 +19,20 @@ func noAuth(c *fiber.Ctx) error {
         log.Fatal("Error when getting session info")
     }
 
-	if strings.Split(c.Path(), "/")[1] == "auth" {
+	if strings.Split(c.Path(), "/")[1] == "auth" && sess.Get(AUTH_KEY) == nil{
+		log.Print("No auth key in cookie")
 		return c.Next()
 	}
 
-    if sess.Get(AUTH_KEY) == nil {
-        log.Print("No auth key in cookie")
-    } 
-
-    return c.Next()
+    return c.Redirect("/user/dashboard")
 }
 
 func checkAuth() fiber.Handler {
 	return auth
 }
 
+// This is to check if user has session AUTH_KEY in their cookie, redirect
+// user to /auth/login if they haven't signed in or session expired
 func auth(c *fiber.Ctx) error {
 	sess, err := store.Get(c)
 
@@ -49,5 +50,5 @@ func auth(c *fiber.Ctx) error {
 		return c.Next()
 	}
 
-	return c.Next()
+	return nil
 }
