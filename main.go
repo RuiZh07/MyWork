2af4edd3e68932bdf5624cf4f862c0f26b296dd8
Ubuntu <cyw@cywang.me>
2023-02-07 19:10:59 +0000

    updated main.go

--- main.go
+++ main.go
@@ -8,18 +8,8 @@ import (
 
 func main() {
 
-	// // log.Println("Waitting for server to boot in 10s")
-	// time.Sleep(10 * time.Second)
-
 	// Start dabase
 	database.Setup()
-
-	// Create table in database
-	// database.CreateTable()
-
-	
-
-	
 
 	// Start web server
 	middleware.Setup()
