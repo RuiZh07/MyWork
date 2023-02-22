b391acc8b46584ac0ec8bda288ac34975715314d
oso95 <cyw@cywang.me>
2023-02-21 22:39:54 -0600

    fixed small bug when display uncreated link in dashboard

--- controller/dashboard.go
+++ controller/dashboard.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"database/sql"
 	"NFC_Tag_UPoint/database"
 	"NFC_Tag_UPoint/model"
 	"fmt"
@@ -20,7 +21,7 @@ func LoadDashboard(c *fiber.Ctx) error {
 	var userName string
 	var userUniversity string
 	var profilePicture string
-	var profileLink string
+	var profileLink sql.NullString
 
 	// Get user name and university from database based on user's email
 	err = database.DB.QueryRow("SELECT name, university, COALESCE(profilePicture, ''), profileLink FROM users WHERE email = $1", userEmail).Scan(&userName, &userUniversity, &profilePicture, &profileLink)
@@ -29,8 +30,14 @@ func LoadDashboard(c *fiber.Ctx) error {
 		log.Fatal(err)
 	}
 
-	domain := c.BaseURL()
-	profileLink = domain + "/" + profileLink
+	if !profileLink.Valid {
+		profileLink.String = "Profile Link Not Set Yet, Go to Profile Page to Set"
+	}else{
+		// If the user has set their profile link, add the domain name to the link
+		domain := c.BaseURL()
+		profileLinkWithDomain := domain + "/" + profileLink.String
+		profileLink.String = profileLinkWithDomain
+	}
 
 	// Check if the user has uploaded their own profile picture
 
@@ -47,7 +54,7 @@ func LoadDashboard(c *fiber.Ctx) error {
 		"ProfilePicture": profilePicture,
 		"UserName":       userName,
 		"UserUniversity": userUniversity,
-		"ProfileLink":    profileLink,
+		"ProfileLink":    profileLink.String,
 	})
 }
 
